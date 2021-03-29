f890a9c7289b42a3ea065407ff8221379cf411bf
tejuafonja <tmafonja1@gmail.com>
2021-03-29 18:43:42 +0000

    update bash script

--- bash_scripts/cmd.sh
+++ bash_scripts/cmd.sh
@@ -1,5 +1,8 @@
-ls en_ng_male/*.wav | awk '{print /home/ubuntu/fac-via-ppg/datasets/}' > en_ng_male.csv
-ls en_ng_female/*.wav | awk '{print /home/ubuntu/fac-via-ppg/datasets/}' > en_ng_female.csv
+PROJECT_ROOT_DIR=~/fac-via-ppg/
+export PYTHONPATH=/home/tejumade/fac-via-ppg//src:/home/tejumade/fac-via-ppg//src:
+
+ls en_ng_male/*.wav | awk '{print "/home/ubuntu/fac-via-ppg/datasets/"$0}' > en_ng_male.csv
+ls en_ng_female/*.wav | awk '{print "/home/ubuntu/fac-via-ppg/datasets/"$0}' > en_ng_female.csv
 . split_train_test.sh en_ng_male.csv male 0.95 filepaths true
 . split_train_test.sh en_ng_female.csv female 0.95 filepaths true
 . split2.sh  filepaths male_train.txt female_train.txt female_valid.txt male_valid.txt
